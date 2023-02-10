fe9856fbedcdfbe249613a9471883814763069a5
romanukalexandr <romanukalexandr@gmail.com>
2023-02-10 15:03:09 +0300

    add Example008 project

---
+++ Example008/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b*b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
