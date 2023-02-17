efe0c153d1c58cb190a463f036dd53435e29c29d
romanukalexandr <romanukalexandr@gmail.com>
2023-02-17 12:39:13 +0300

    create 014 project

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -4,7 +4,7 @@ int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = int.Parse(Console.ReadLine());
 
-if (a == b*b)
+if (a == b*b || b == a*a)
 {
     Console.WriteLine("Да");
 }
---
+++ Example013NullTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите целое исло");
+int a = int.Parse (Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine ("Число кратно одновремено 7 и 23");
+}
+else
+{
+    Console.WriteLine ("Число не кратно одновремено 7 и 23");
+}
