92f0a62f286c58043300d0777fbd6a8b304b437c
romanukalexandr <romanukalexandr@gmail.com>
2023-02-21 16:58:51 +0300

    add 019 FindInSite project

---
+++ Example019/Program.cs
@@ -0,0 +1,19 @@
+﻿string GetHtml(string url, string path)
+{
+    string html = String.Empty;
+    if (!File.Exists(path))
+    {
+        html = new HttpClient().GetStringAsync(url).Result;
+        File.WriteAllText(path, html);
+    }
+    else
+    {
+        html = File.ReadAllText(path);
+    }
+    return html;
+}
+string url = "https://m.vk.com";
+string localPath = "vkcom.html";
+string html = GetHtml(url, localPath);
+System.Console.WriteLine(html);
+System.Console.WriteLine(s.IndexOf("cjsdf"));
