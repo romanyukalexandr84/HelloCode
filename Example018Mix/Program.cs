ef6f6182cd683c150efbb4753afb8652cc25f4ce
romanukalexandr <romanukalexandr@gmail.com>
2023-02-21 05:17:54 +0300

    add 018Mix project

---
+++ Example018Mix/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] arr = {1, 8, 9, 5 ,2, 6, 4, 1, 15};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine ();
+}
+
+void MixArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int j = new Random().Next (0, array.Length);
+        Console.Write($"{array[j]} ");
+    }
+}
+
+PrintArray (arr);
+MixArray (arr);
+Console.WriteLine ();
